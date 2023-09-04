02b53e87eadd1e56c3fca3eef05769e6d3117673
Chokitera <gapapp136@gmail.com>
2023-09-04 19:57:21 -0300

    Ajustes realizados no Login

--- Rika/Form1.Designer.cs
+++ Rika/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.txtUsuario = new Rika.controls.novoTextBox();
             this.txtSenha = new Rika.controls.novoTextBox();
             this.btnLogin = new Rika.controls.ModeloBotao();
-            this.lblAviso = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,7 +41,7 @@
             // 
             this.lblUser.AutoSize = true;
             this.lblUser.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUser.Location = new System.Drawing.Point(82, 189);
+            this.lblUser.Location = new System.Drawing.Point(69, 189);
             this.lblUser.Name = "lblUser";
             this.lblUser.Size = new System.Drawing.Size(88, 30);
             this.lblUser.TabIndex = 0;
@@ -52,7 +51,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Rika.Properties.Resources.Logo_Rika_Preto;
-            this.pictureBox1.Location = new System.Drawing.Point(177, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(152, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(192, 134);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -63,7 +62,7 @@
             // 
             this.lblSenha.AutoSize = true;
             this.lblSenha.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSenha.Location = new System.Drawing.Point(82, 236);
+            this.lblSenha.Location = new System.Drawing.Point(69, 236);
             this.lblSenha.Name = "lblSenha";
             this.lblSenha.Size = new System.Drawing.Size(73, 30);
             this.lblSenha.TabIndex = 2;
@@ -77,7 +76,7 @@
             this.txtUsuario.BorderSize = 2;
             this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtUsuario.ForeColor = System.Drawing.Color.DimGray;
-            this.txtUsuario.Location = new System.Drawing.Point(177, 189);
+            this.txtUsuario.Location = new System.Drawing.Point(164, 189);
             this.txtUsuario.Margin = new System.Windows.Forms.Padding(4);
             this.txtUsuario.Multiline = false;
             this.txtUsuario.Name = "txtUsuario";
@@ -86,6 +85,7 @@
             this.txtUsuario.Size = new System.Drawing.Size(250, 31);
             this.txtUsuario.TabIndex = 3;
             this.txtUsuario.UnderlinedStyle = false;
+            this.txtUsuario._TextChanged += new System.EventHandler(this.txtUsuario__TextChanged);
             // 
             // txtSenha
             // 
@@ -95,7 +95,7 @@
             this.txtSenha.BorderSize = 2;
             this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtSenha.ForeColor = System.Drawing.Color.DimGray;
-            this.txtSenha.Location = new System.Drawing.Point(177, 236);
+            this.txtSenha.Location = new System.Drawing.Point(164, 236);
             this.txtSenha.Margin = new System.Windows.Forms.Padding(4);
             this.txtSenha.Multiline = false;
             this.txtSenha.Name = "txtSenha";
@@ -115,7 +115,7 @@
             this.btnLogin.FlatAppearance.BorderSize = 0;
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(186, 302);
+            this.btnLogin.Location = new System.Drawing.Point(176, 306);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(150, 40);
             this.btnLogin.TabIndex = 5;
@@ -124,22 +124,12 @@
             this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
-            // lblAviso
-            // 
-            this.lblAviso.AutoSize = true;
-            this.lblAviso.Location = new System.Drawing.Point(271, 271);
-            this.lblAviso.Name = "lblAviso";
-            this.lblAviso.Size = new System.Drawing.Size(35, 13);
-            this.lblAviso.TabIndex = 6;
-            this.lblAviso.Text = "label1";
-            this.lblAviso.Visible = false;
-            // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(503, 368);
-            this.Controls.Add(this.lblAviso);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtSenha);
             this.Controls.Add(this.txtUsuario);
@@ -149,7 +139,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.Name = "FrmLogin";
-            this.Text = "Form1";
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -165,7 +155,6 @@
         private controls.novoTextBox txtUsuario;
         private controls.novoTextBox txtSenha;
         private controls.ModeloBotao btnLogin;
-        private System.Windows.Forms.Label lblAviso;
     }
 }
 
--- Rika/Form1.cs
+++ Rika/Form1.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,6 +19,21 @@ namespace Rika
         {
             InitializeComponent();
         }
+
+        #region Ajustes da Parte Superior do Formulário
+        //Dlls para arrastar o Formulário
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void FrmLogin_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Chamada dos métodos para arrastar o formulário
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        #endregion
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -44,6 +60,11 @@ namespace Rika
             {
                 //Chamaria a tela inicial
             }
+        }
+
+        private void txtUsuario__TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
