64c41325b86c72fe8c2b861ce8ca777b03fa0ee6
Richard Nicoletto <richardnicoletti08@gmail.com>
2023-09-26 19:35:07 -0300

    Criado as views das telas de cadastros

---
+++ Rika/views/Classe.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Rika.views
+{
+    partial class Classe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.novoTextBox3 = new Rika.controls.novoTextBox();
+            this.novoTextBox2 = new Rika.controls.novoTextBox();
+            this.novoTextBox1 = new Rika.controls.novoTextBox();
+            this.btnSair = new Rika.controls.ModeloBotao();
+            this.btnExcluir = new Rika.controls.ModeloBotao();
+            this.btnSalvar = new Rika.controls.ModeloBotao();
+            this.lblAeroDestino = new System.Windows.Forms.Label();
+            this.lblAeroDecolagem = new System.Windows.Forms.Label();
+            this.label1lblCodigoPassagem = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // novoTextBox3
+            // 
+            this.novoTextBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.novoTextBox3.BorderColor = System.Drawing.Color.DimGray;
+            this.novoTextBox3.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.novoTextBox3.BorderSize = 1;
+            this.novoTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.novoTextBox3.ForeColor = System.Drawing.Color.DimGray;
+            this.novoTextBox3.Location = new System.Drawing.Point(257, 286);
+            this.novoTextBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.novoTextBox3.Multiline = false;
+            this.novoTextBox3.Name = "novoTextBox3";
+            this.novoTextBox3.Padding = new System.Windows.Forms.Padding(7);
+            this.novoTextBox3.PasswordChar = false;
+            this.novoTextBox3.Size = new System.Drawing.Size(250, 31);
+            this.novoTextBox3.TabIndex = 66;
+            this.novoTextBox3.TextNew = "";
+            this.novoTextBox3.UnderlinedStyle = false;
+            // 
+            // novoTextBox2
+            // 
+            this.novoTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.novoTextBox2.BorderColor = System.Drawing.Color.DimGray;
+            this.novoTextBox2.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.novoTextBox2.BorderSize = 1;
+            this.novoTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.novoTextBox2.ForeColor = System.Drawing.Color.DimGray;
+            this.novoTextBox2.Location = new System.Drawing.Point(257, 247);
+            this.novoTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.novoTextBox2.Multiline = false;
+            this.novoTextBox2.Name = "novoTextBox2";
+            this.novoTextBox2.Padding = new System.Windows.Forms.Padding(7);
+            this.novoTextBox2.PasswordChar = false;
+            this.novoTextBox2.Size = new System.Drawing.Size(250, 31);
+            this.novoTextBox2.TabIndex = 65;
+            this.novoTextBox2.TextNew = "";
+            this.novoTextBox2.UnderlinedStyle = false;
+            // 
+            // novoTextBox1
+            // 
+            this.novoTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.novoTextBox1.BorderColor = System.Drawing.Color.DimGray;
+            this.novoTextBox1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.novoTextBox1.BorderSize = 1;
+            this.novoTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.novoTextBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.novoTextBox1.Location = new System.Drawing.Point(257, 208);
+            this.novoTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.novoTextBox1.Multiline = false;
+            this.novoTextBox1.Name = "novoTextBox1";
+            this.novoTextBox1.Padding = new System.Windows.Forms.Padding(7);
+            this.novoTextBox1.PasswordChar = false;
+            this.novoTextBox1.Size = new System.Drawing.Size(250, 31);
+            this.novoTextBox1.TabIndex = 64;
+            this.novoTextBox1.TextNew = "";
+            this.novoTextBox1.UnderlinedStyle = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnSair.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnSair.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnSair.BorderRadius = 20;
+            this.btnSair.BorderSize = 0;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.ForeColor = System.Drawing.Color.White;
+            this.btnSair.Location = new System.Drawing.Point(357, 335);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(150, 45);
+            this.btnSair.TabIndex = 55;
+            this.btnSair.Text = "Sair";
+            this.btnSair.TextColor = System.Drawing.Color.White;
+            this.btnSair.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.Red;
+            this.btnExcluir.BackgroundColor = System.Drawing.Color.Red;
+            this.btnExcluir.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnExcluir.BorderRadius = 20;
+            this.btnExcluir.BorderSize = 0;
+            this.btnExcluir.FlatAppearance.BorderSize = 0;
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.ForeColor = System.Drawing.Color.White;
+            this.btnExcluir.Location = new System.Drawing.Point(196, 335);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(150, 45);
+            this.btnExcluir.TabIndex = 54;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.TextColor = System.Drawing.Color.White;
+            this.btnExcluir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnSalvar.BackgroundColor = System.Drawing.Color.LimeGreen;
+            this.btnSalvar.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnSalvar.BorderRadius = 20;
+            this.btnSalvar.BorderSize = 0;
+            this.btnSalvar.FlatAppearance.BorderSize = 0;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(36, 335);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(150, 45);
+            this.btnSalvar.TabIndex = 53;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.TextColor = System.Drawing.Color.White;
+            this.btnSalvar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            // 
+            // lblAeroDestino
+            // 
+            this.lblAeroDestino.AutoSize = true;
+            this.lblAeroDestino.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAeroDestino.Location = new System.Drawing.Point(36, 286);
+            this.lblAeroDestino.Name = "lblAeroDestino";
+            this.lblAeroDestino.Size = new System.Drawing.Size(90, 22);
+            this.lblAeroDestino.TabIndex = 47;
+            this.lblAeroDestino.Text = "Descrição";
+            // 
+            // lblAeroDecolagem
+            // 
+            this.lblAeroDecolagem.AutoSize = true;
+            this.lblAeroDecolagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAeroDecolagem.Location = new System.Drawing.Point(36, 247);
+            this.lblAeroDecolagem.Name = "lblAeroDecolagem";
+            this.lblAeroDecolagem.Size = new System.Drawing.Size(62, 22);
+            this.lblAeroDecolagem.TabIndex = 46;
+            this.lblAeroDecolagem.Text = "Nome:";
+            // 
+            // label1lblCodigoPassagem
+            // 
+            this.label1lblCodigoPassagem.AccessibleName = "";
+            this.label1lblCodigoPassagem.AutoSize = true;
+            this.label1lblCodigoPassagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1lblCodigoPassagem.Location = new System.Drawing.Point(36, 208);
+            this.label1lblCodigoPassagem.Name = "label1lblCodigoPassagem";
+            this.label1lblCodigoPassagem.Size = new System.Drawing.Size(157, 22);
+            this.label1lblCodigoPassagem.TabIndex = 45;
+            this.label1lblCodigoPassagem.Text = "Código da Classe:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Rika.Properties.Resources.Logo_Rika_Preto;
+            this.pictureBox1.Location = new System.Drawing.Point(131, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 44;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Classe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(517, 394);
+            this.Controls.Add(this.novoTextBox3);
+            this.Controls.Add(this.novoTextBox2);
+            this.Controls.Add(this.novoTextBox1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.lblAeroDestino);
+            this.Controls.Add(this.lblAeroDecolagem);
+            this.Controls.Add(this.label1lblCodigoPassagem);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Classe";
+            this.Text = "Classe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private controls.novoTextBox novoTextBox3;
+        private controls.novoTextBox novoTextBox2;
+        private controls.novoTextBox novoTextBox1;
+        private controls.ModeloBotao btnSair;
+        private controls.ModeloBotao btnExcluir;
+        private controls.ModeloBotao btnSalvar;
+        private System.Windows.Forms.Label lblAeroDestino;
+        private System.Windows.Forms.Label lblAeroDecolagem;
+        private System.Windows.Forms.Label label1lblCodigoPassagem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
