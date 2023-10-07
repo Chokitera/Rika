6eafe89712bd1558912ebb5a732b52e10482ea51
Isaac <Isaac23gb@gmail.com>
2023-10-07 13:11:58 -0300

    Model Endereço criado

---
+++ Rika/models/Endereço.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rika.models
+{
+    internal class Endereço : Entidade
+    {
+        public Pais pais = new Pais();
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public int CEP { get; set; }
+        public string Logradouro { get; set; }
+        public int NumeroCasa { get; set; }
+        public string Complemento { get; set; }
+    }
+}
